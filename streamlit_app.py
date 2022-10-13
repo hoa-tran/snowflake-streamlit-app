ec8dc091896c26d3129065384a58871b487cc121
hoa-tran <hoatran912@gmail.com>
2022-10-13 14:46:13 +0200

    update streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,9 @@ fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/
 
 fruit_list = fruit_list.set_index('Fruit')
 
-st.multiselect("Pick some fruits:", list(fruit_list.index))
+fruits_selected = st.multiselect("Pick some fruits:", list(fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = fruit_list.loc[fruits_selected]
 
-st.dataframe(fruit_list)
+
+st.dataframe(fruits_to_show)
 
