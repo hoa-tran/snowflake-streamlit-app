95641006976f111eca55b321ca6413c165874cf7
hoa-tran <hoatran912@gmail.com>
2022-10-15 16:01:51 +0200

    update streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import requests
 import snowflake.connector
+from urllib.error import URLError
+
 
 st.title("My Parents New Healthy Dinner")
 
@@ -20,17 +22,24 @@ fruit_list = fruit_list.set_index('Fruit')
 
 fruits_selected = st.multiselect("Pick some fruits:", list(fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = fruit_list.loc[fruits_selected]
-
-
 st.dataframe(fruits_to_show)
 
-st.header("Fruityvice Fruit Advice!")
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-st.dataframe(fruityvice_normalized)
 
+
+
+st.header("Fruityvice Fruit Advice!")
+try:
+    fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+    if not fruit_choice:
+        st.error("Please select a fruit to get information")
+    else:
+
+        st.write('The user entered ', fruit_choice)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        st.dataframe(fruityvice_normalized)
+except URLError as e:
+    st.error()
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
